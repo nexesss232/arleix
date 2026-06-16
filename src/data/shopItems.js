61476402a7b97928a71297f2390c77d56fabe1ff
nexesss232 <popovicha669@gmail.com>
2026-06-16 20:09:01 +0300

    Create shopItems.js

---
+++ src/data/shopItems.js
@@ -0,0 +1,7 @@
+export const SHOP_ITEMS = [
+  { id: "tap1", name: "+1 Tap", price: 100, type: "tap", value: 1 },
+  { id: "tap2", name: "+5 Tap", price: 500, type: "tap", value: 5 },
+  { id: "auto1", name: "+1/sec", price: 800, type: "auto", value: 1 },
+  { id: "crit1", name: "+10% Crit", price: 1200, type: "crit", value: 0.1 },
+  { id: "multi1", name: "x2 Multiplier", price: 2000, type: "multi", value: 2 },
+];
