c0bfd347b9ff6a76c9ffa52ebba269923c279cf5
nexesss232 <popovicha669@gmail.com>
2026-06-16 20:09:00 +0300

    Create skins.js

---
+++ src/data/skins.js
@@ -0,0 +1,5 @@
+export const SKINS = [
+  { id: "default", name: "Gold", color: "#facc15" },
+  { id: "blue", name: "Blue Neon", color: "#38bdf8" },
+  { id: "purple", name: "Purple", color: "#a855f7" },
+];
