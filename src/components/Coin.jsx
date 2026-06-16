f38aa9b41a06bfe68c3150f05f2edda8acb9769d
nexesss232 <popovicha669@gmail.com>
2026-06-16 19:07:19 +0300

    Update Coin.jsx

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -1,25 +1,14 @@
-import { useState } from "react";
 import coinImg from "../assets/pngwing.com.png";
 
 export default function Coin({ onClick }) {
-  const [anim, setAnim] = useState(false);
-
-  function handleClick() {
-    setAnim(true);
-    setTimeout(() => setAnim(false), 100);
-
-    onClick();
-  }
-
   return (
     <img
       src={coinImg}
-      onClick={handleClick}
+      onClick={onClick}
       style={{
         width: 160,
         cursor: "pointer",
-        transform: anim ? "scale(1.2)" : "scale(1)",
-        transition: "0.1s",
+        userSelect: "none",
       }}
     />
   );
